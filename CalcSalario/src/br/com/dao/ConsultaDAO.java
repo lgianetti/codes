bb406309c13931584738c520dca41bb09a1c1556
lgianetti <luis_gianetti@hotmail.com>
2014-05-26 14:31:11 -0300

    Criaçao da classe ConsultaDAO

---
+++ CalcSalario/src/br/com/dao/ConsultaDAO.java
@@ -0,0 +1,81 @@
+package br.com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import br.com.bean.Desconto;
+import br.com.bean.Funcionario;
+
+public class ConsultaDAO {
+	/*
+	 * Consulta salario bruto mais os descontos, esta consulta nao pode retornar o salario liquido
+	 * sem calculos, os calculos devem ser feitos via codigo.
+	 * */
+	private String QUERY = "SELECT F.ID_FUNCIONARIO AS ID_FUNCIONARIO, F.VL_SALARIO_BRUTO AS VL_SALARIO_BRUTO AS VL_SALARIO_BRUTO"
+			+ ", D.ID_DESCONTO AS ID_DESCONTO, D.VL_DESCONTO AS VL_DESCONTO"
+			+ " FROM FUNCIONARIO F RIGHT JOIN DESCONTO D ON  F.ID_FUNCIONARIO = D.ID_FUNCIONARIO";
+	
+	private ArrayList<Funcionario> funcionarios;
+	private ArrayList<Desconto> descontos;
+	
+	private Connection connection;
+	
+	private PreparedStatement preparedStatement;
+	private ResultSet resultSet;
+	
+	private Funcionario funcionario;
+	
+	public ArrayList<Funcionario> getListaDescontos(){
+		funcionario = new Funcionario();
+		int flag = 0;
+		
+		funcionarios = new ArrayList<Funcionario>();
+		descontos = new ArrayList<Desconto>();
+		
+		connection = new ConnectionFactory.getConnection();
+		
+		if(connection != null){
+			try{
+			preparedStatement = connection.prepareStatement(QUERY);
+			resultSet = preparedStatement.executeQuery();
+				while(resultSet.next()){
+					if(Integer.parseInt(resultSet.getString("ID_FUNCIONARIO")) == flag){
+						Desconto desconto = new Desconto();
+						desconto.setId_desconto(resultSet.getInt("ID_DESCONTO"));
+						desconto.setVl_desconto(resultSet.getDouble("VL_DESCONTO"));
+						descontos.add(desconto);					
+					}else{
+						funcionario = new Funcionario();
+						flag = Integer.parseInt(resultSet.getString("ID_FUNCIONARIO"));
+						
+						funcionario.setId_funcionario(flag);
+						funcionario.setVl_salario_bruto(resultSet.getDouble("VL_SALARIO_BRUTO"));
+						
+						descontos = new ArrayList<Desconto>();
+						Desconto desconto = new Desconto();
+						
+						desconto.setId_desconto(resultSet.getInt("ID_DESCONTO"));
+						desconto.setVl_desconto(resultSet.getDouble("VL_DESCONTO"));
+					
+						descontos.add(desconto);
+						
+						funcionario.setDescontos(descontos);
+						funcionarios.add(funcionario);
+								
+					}
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}finally{
+				connection.close();
+			}
+			return funcionarios;
+		}
+		
+	}
+	
+	
+}
