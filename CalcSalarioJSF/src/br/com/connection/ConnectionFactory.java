11c5469977ab3e4197c249cb6803b402443b97d2
lgianetti <luis_gianetti@hotmail.com>
2014-05-28 18:00:04 -0300

    Criaçao dos testes da classe ConnectionFactory

--- CalcSalarioJSF/src/br/com/connection/ConnectionFactory.java
+++ CalcSalarioJSF/src/br/com/connection/ConnectionFactory.java
@@ -1,11 +1,34 @@
 package br.com.connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+	private String SERVER_NAME = "127.0.0.1";
+	private String PORT_NUMBER = "1521";
+	private String SID = "SW01";
+	
+	private String URL = "jdbc:oracle:thin:@" + SERVER_NAME + ":" + PORT_NUMBER + ":" + SID;
+	
+	private String USERNAME = "oracle";
+	private String PASSWORD = "dba";
+	
+	static{
+		try {
+			//Carrega o driver JDBC
+			String driverName = "oracle.jdbc.driver.OracleDriver";
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * @return retorna um objeto do tipo java.sql.Connection
+	 * */
 	public Connection getConnection() throws SQLException{
-		return null;
+		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
 	}
 }
--- CalcSalarioJSF/src/br/com/tests/connection/ConnectionFactoryTest.java
+++ CalcSalarioJSF/src/br/com/tests/connection/ConnectionFactoryTest.java
@@ -1,23 +1,44 @@
 package br.com.tests.connection;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.com.connection.ConnectionFactory;
 
+
+
+
 public class ConnectionFactoryTest {
 
-	@Test(expected=SQLException.class)
-	public void getConnection() throws SQLException{
-		 try {
-			 ConnectionFactory factory = new ConnectionFactory();
-			 factory.getConnection();
-			 Assert.fail("Expected SQL Exception was not thrown");
-		    } catch (SQLException e) {
-		    	Assert.assertEquals(21, e.getErrorCode());
-		    }
+	@BeforeClass
+	public static void forName() throws Exception {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+    }
+	
+	 @Test(expected=SQLException.class)
+	public void createConnection() throws SQLException {
+	    Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:SW01");
+	    conn.close();
 	}
+	 
+	@Test
+	public void executeConnection() throws SQLException {
+	    Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:SW01", "oracle", "dba");
+	    conn.close();
+	} 
+	
+	
+	@Test
+	public void createFactoryConnection() throws SQLException {
+	    ConnectionFactory factory = new ConnectionFactory();
+	    assertNotNull(factory.getConnection());
+	} 
 }
